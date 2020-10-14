6b287b4b721888803627211c68475f90e7e0213f
Mohammadtalha <mohammadtalha.patel@thegatewaycorp.com>
2020-10-14 18:27:02 +0530

    description added

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         test: /\.js$/, //tell webpack to look for all .js files
         exclude: /node_modules/, //excluding the files inside node_modules for the purpose of transpiling ES5, ES6 codes
         use: {
-          loader: "babel-loader", //webpack use babel-loader for the above purpose
+          loader: "babel-loader", //webpack use babel-loader so that is not all browser support ES6 so it translate the code into vanila JS so that browser can undestand
         },
       },
     ], //Rules: these are the rules which bind with webpack
@@ -25,3 +25,7 @@ module.exports = {
     }),
   ],
 };
+
+//we require babel because webpack can merge all data into 1 single file but it will not translate the code into vanila js
+// for example : we have written like const obj = "talha" so webpack put as it is in bundle.js but we need to change it like "var" so we use babel-loader and preset for that
+//babel-preset means what need to transform in vanila js
